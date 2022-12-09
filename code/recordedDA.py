c997aaa840d6337667773407a54ad1151580d72c
Anthony Selim <anthony.af.selim@gmail.com>
2022-12-09 18:05:20 +0200

    fixes for obst and recorded mode done

--- code/recordedDA.py
+++ code/recordedDA.py
@@ -1,5 +1,8 @@
+import cv2 # OpenCV for perspective transform
 import numpy as np
-import cv2
+import glob  # For reading in a list of images from a folder
+import csv
+import matplotlib.image as mpimg
 
 # Identify pixels above the threshold
 # Threshold of RGB > 160 does a nice job of identifying ground pixels only
@@ -104,13 +107,21 @@ def rock_thresh(img, rgb_thresh=(95, 95, 20)):
     return color_select
 
 
-# Apply the above functions in succession and update the Rover state accordingly
-def perception_step(Rover):
-    # Perform perception steps to update Rover()
-    # TODO: 
-    # NOTE: camera image is coming to you in Rover.img
-    image = Rover.img
-    # 1) Define source and destination points for perspective transform
+csvpath = '../test_dataset/robot_log.csv'
+ground_truth = mpimg.imread('../calibration_images/map_bw.png')
+ground_truth = cv2.merge((ground_truth,ground_truth,ground_truth))*0
+
+with open(csvpath, 'r') as file:
+  csvreader = list(csv.reader(file, delimiter=';'))
+  
+  for row in csvreader[1:]:
+    impath = row[0]
+    imxp = np.float64(row[5])
+    imyp = np.float64(row[6])
+    imyaw = np.float64(row[8])
+    image = mpimg.imread(impath)
+    vision_image = cv2.imread(impath)
+    
     dst_s = 5
     bottom_offset = 5
     
@@ -138,14 +149,10 @@ def perception_step(Rover):
     rocks = rock_thresh(warped)
 
     # 4) Update Rover.vision_image (this will be displayed on left side of screen)
-        # Example: Rover.vision_image[:,:,0] = obstacle color-thresholded binary image
-        #          Rover.vision_image[:,:,1] = rock_sample color-thresholded binary image
-        #          Rover.vision_image[:,:,2] = navigable terrain color-thresholded binary image
 
-    Rover.vision_image[:,:,2] = color_thresh(image)*255
-    Rover.vision_image[:,:,0] = obst_thresh(image)*255
-    Rover.vision_image[:,:,1] = rock_thresh(image)*255
-
+    vision_image[:,:,2] = color_thresh(image)*255
+    vision_image[:,:,0] = obst_thresh(image)*255
+    vision_image[:,:,1] = rock_thresh(image)*255
 
     # 5) Convert map image pixel values to rover-centric coords
 
@@ -156,67 +163,51 @@ def perception_step(Rover):
     # 6) Convert rover-centric pixel values to world coordinates
 
     dist, angles = to_polar_coords(xp, yp)
-    worldmap = Rover.worldmap
 
     # 7) Update Rover worldmap (to be displayed on right side of screen)
-        # Example: Rover.worldmap[obstacle_y_world, obstacle_x_world, 0] += 1
-        #          Rover.worldmap[rock_y_world, rock_x_world, 1] += 1
-        #          Rover.worldmap[navigable_y_world, navigable_x_world, 2] += 1
 
-    navigable_x_world, navigable_y_world = pix_to_world(xp,yp,Rover.pos[0],Rover.pos[1],Rover.yaw,worldmap.shape[0],2*dst_s)
-    Rover.worldmap[navigable_y_world, navigable_x_world, 2] = 255
-    Rover.worldmap[navigable_y_world, navigable_x_world, 0] = 0
+    obstacle_x_world, obstacle_y_world = pix_to_world(oxp,oyp,imxp,imyp,imyaw,ground_truth.shape[0],2*dst_s)
+    ground_truth[obstacle_y_world, obstacle_x_world, 2] = 255
 
-    obstacle_x_world, obstacle_y_world = pix_to_world(oxp,oyp,Rover.pos[0],Rover.pos[1],Rover.yaw,worldmap.shape[0],2*dst_s)
-    Rover.worldmap[obstacle_y_world, obstacle_x_world, 0] = 255
+    rock_x_world, rock_y_world = pix_to_world(rxp,ryp,imxp,imyp,imyaw,ground_truth.shape[0],2*dst_s)
+    ground_truth[rock_y_world, rock_x_world, 1] = 255
 
-    rock_x_world, rock_y_world = pix_to_world(rxp,ryp,Rover.pos[0],Rover.pos[1],Rover.yaw,worldmap.shape[0],2*dst_s)
-    Rover.worldmap[rock_y_world, rock_x_world, 1] = 255
-
-
-    # 8) Convert rover-centric pixel positions to polar coordinates
-    # Update Rover pixel distances and angles
-        # Rover.nav_dists = rover_centric_pixel_distances
-        # Rover.nav_angles = rover_centric_angles
+    navigable_x_world, navigable_y_world = pix_to_world(xp,yp,imxp,imyp,imyaw,ground_truth.shape[0],2*dst_s)
+    ground_truth[navigable_y_world, navigable_x_world, 0] = 255
+    ground_truth[navigable_y_world, navigable_x_world, 2] = 0
     
-    Rover.nav_dists = dist
-    Rover.nav_angles = angles
 
-    # 9) Debugging Mode
-    ######## SET TO TRUE IF YOU WANT DEBUGGING MODE ACTIVE
-
-    dbugmode = True
-
-    if dbugmode:
-        arrow_length = 100
-        mean_dir = np.mean(angles)
-        x_arrow = arrow_length * np.cos(mean_dir)
-        y_arrow = arrow_length * np.sin(mean_dir)
-        try:
-            cv2.imshow('Original Image', image)
-            cv2.imshow('Warped Image', warped)
-            cv2.imshow('Navigatabile Warped Terrain Image', threshed*255)
-            cv2.imshow('Obstical Warpeed Terrain Image', obstic*255)
-            cv2.imshow('Rock Warped Terrain Image', rocks*255)
-            pimg = np.zeros((321,161,3), np.uint8)
-            oxpi = np.int_(oxp)
-            oypi = np.int_(oyp)
-            for i in range(len(oxpi)):
-                pimg = cv2.circle(pimg, (oxpi[i],oypi[i]+160), radius=0, color=(0,0,255), thickness=1)
-            rxpi = np.int_(rxp)
-            rypi = np.int_(ryp)
-            for i in range(len(rxpi)):
-                pimg = cv2.circle(pimg, (rxpi[i],rypi[i]+160), radius=0, color=(0,255,0), thickness=1)
-            xpi = np.int_(xp)
-            ypi = np.int_(yp)
-            for i in range(len(xpi)):
-                pimg = cv2.circle(pimg, (xpi[i],ypi[i]+160), radius=0, color=(255,0,0), thickness=1)
-            pimg = cv2.line(pimg, (0,160), (int(x_arrow), int(y_arrow)+160), color=(255,255,255), thickness=5)
-            cv2.imshow("Polar Image", pimg)
-            cv2.waitKey(1)
-        except:
-            print("no blues")
+    arrow_length = 100
+    mean_dir = np.mean(angles)
+    x_arrow = arrow_length * np.cos(mean_dir)
+    y_arrow = arrow_length * np.sin(mean_dir)
+    try:
+        cv2.imshow('Original Image', image)
+        cv2.imshow('Warped Image', warped)
+        cv2.imshow('Navigatabile Warped Terrain Image', threshed*255)
+        cv2.imshow('Obstical Warpeed Terrain Image', obstic*255)
+        cv2.imshow('Rock Warped Terrain Image', rocks*255)
+        pimg = np.zeros((321,161,3), np.uint8)
+        oxpi = np.int_(oxp)
+        oypi = np.int_(oyp)
+        for i in range(len(oxpi)):
+            pimg = cv2.circle(pimg, (oxpi[i],oypi[i]+160), radius=0, color=(0,0,255), thickness=1)
+        rxpi = np.int_(rxp)
+        rypi = np.int_(ryp)
+        for i in range(len(rxpi)):
+            pimg = cv2.circle(pimg, (rxpi[i],rypi[i]+160), radius=0, color=(0,255,0), thickness=1)
+        xpi = np.int_(xp)
+        ypi = np.int_(yp)
+        for i in range(len(xpi)):
+            pimg = cv2.circle(pimg, (xpi[i],ypi[i]+160), radius=0, color=(255,0,0), thickness=1)
+        pimg = cv2.line(pimg, (0,160), (int(x_arrow), int(y_arrow)+160), color=(255,255,255), thickness=5)
+        cv2.imshow("Polar Image", pimg)
         
-        
-    
-    return Rover
+    except:
+        print("no blues")
+
+
+    cv2.imshow('Map Image', ground_truth)
+    cv2.imshow('Vision Image', vision_image)
+    cv2.waitKey(10)
+
